82c931f2072f502ece2689dec095e0ae7af1b5a8
will greer <willfromspace@gmail.com>
2021-10-22 16:15:15 -0700

    submodule for bspwm

---
+++ scripts/hide.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#  D=$(bspc query –desktops –desktop focused);
+#  W=$(bspc query –windows –window focused);
+#  bspc query –desktops | grep -q “‘$D” || bspc monitor –add-desktops '$D;
+#  bspc window $W --to-desktop \'$D
+
+#  D=$(bspc query –desktops –desktop focused);
+#  W=$(bspc query –windows –window focused);
+#  bspc query –desktops | grep -q “‘$D” || bspc monitor –add-desktops '$D;
+#  bspc w
+
+hidden=$(bspc query -N -n .hidden -d focused)
+
+if [ -z "$hidden" ]; then
+	bspc node focused -g hidden=on
+else
+	bspc node "$hidden" -g hidden=off
+fi
