635ba326847320b3f257fa695e515410ac25b3ea
will greer <willfromspace@gmail.com>
2022-02-02 23:14:42 -0700

    revert some older changes after system failed to start

--- scripts/multiple-monitors.sh
+++ scripts/multiple-monitors.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --brightness .80 --mode 1920x1080 --pos 0x251 --rate 165.00 --rotate normal --output HDMI-A-0 --off --output HDMI-A-1 --off --output DVI-D-0 --mode 1920x1080 --brightness 0.60 --right-of DisplayPort-1 --rotate normal
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --brightness .80 --mode 1920x1080 --pos 0x251 --rate 165.00 --rotate normal --output HDMI-A-0 --off --output HDMI-A-1 --off --output DVI-D-0 --mode 1920x1080 --brightness .95 --right-of DisplayPort-1 --rotate normal
